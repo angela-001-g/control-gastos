5c78634b1cfa85b2c7d77bd5ecbba4413a1dcad4
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-01 14:50:53 -0500

    first components

--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -1,12 +1,12 @@
-
+import Header from "./components/Header"
 
 function App() {
  
-
   return (
-    <>
-      <h1>Control de gastos</h1>
-    </>
+    
+      <div>
+        <Header />
+      </div>
   )
 }
 
