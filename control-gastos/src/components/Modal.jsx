f43f594e197a3385eddfca3398b34aeff066c79b
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-02 14:58:38 -0500

    state para gastos

--- control-gastos/src/components/Modal.jsx
+++ control-gastos/src/components/Modal.jsx
@@ -1,7 +1,14 @@
+import {useState} from 'react'
 import CerrarBtn from "../img/cerrar.svg"
 
 // eslint-disable-next-line react/prop-types
 function Modal({ setModal, animarModal, setAnimarModal }) {
+
+    const[nombre, setNombre] = useState('')
+    const[cantidad, setCantidad] = useState('')
+    const[categoria, setCategoria] = useState('')
+
+
 
     const ocultarModal = () => {
         setAnimarModal(false)
@@ -30,6 +37,8 @@ function Modal({ setModal, animarModal, setAnimarModal }) {
                 id="nombre"
                 type="text" 
                 placeholder="Añade el nombre del gasto"
+                value={nombre}
+                onChange={ e => setNombre(e.target.value)}
             />
         </div>
 
@@ -39,13 +48,19 @@ function Modal({ setModal, animarModal, setAnimarModal }) {
                 id="cantidad"
                 type="number" 
                 placeholder="Añade la cantidad del gasto: ej. 300"
+                value={cantidad}
+                onChange={e => setCantidad(Number(e.target.value))}
             />
         </div>
 
         <div className="campo">
             <label htmlFor="categoria">Categoria</label>
             
-            <select id="categoria">
+            <select 
+                id="categoria"
+                value={categoria}  
+                onChange={e => setCategoria(e.target.value)}  
+            >
 
                 <option value="">-- Seleccione --</option>
                 <option value="ahorro">Ahorro</option>
