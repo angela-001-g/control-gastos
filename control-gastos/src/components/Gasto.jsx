a34a2f98692819315638241d5360bea86e838ac0
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-08 22:07:35 -0500

    swipe action

--- control-gastos/src/components/Gasto.jsx
+++ control-gastos/src/components/Gasto.jsx
@@ -31,19 +31,27 @@ function Gasto({gasto}) {
 
     const {categoria, nombre, cantidad, fecha} = gasto 
 
-    const leadingActions = () => {
-      console.log('Editar...') 
-    }
+    const leadingActions = () => (
+      <LeadingActions>
+        <SwipeAction onClick={() => console.log('Editar...')}>
+            Editar
+        </SwipeAction>
+      </LeadingActions>
+    )
 
-    const trailingActions = () => {
-      console.log('Eliminar...')
-    }
+    const trailingActions = () => (
+      <TrailingActions>
+        <SwipeAction onClick={() => console.log('Eliminar...')}>
+            Eliminar
+        </SwipeAction>
+      </TrailingActions>
+    )
 
   return (
     <SwipeableList> 
       <SwipeableListItem
-        leadingActions={leadingActions}
-        trailingActions={trailingActions}
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
       >
         <div className="gasto sombra">
           <div className="contenido-gasto">
